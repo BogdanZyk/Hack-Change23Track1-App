fab17781d0069b22baf0c032b9d3a8d5b5223d2c
BogdnaZyk <zykovbogdan07@gmail.com>
2023-11-24 13:52:22 +0300

    added UserDataService class

---
+++ Hack&Change23Track1/Services/ApiServices/User/UserDataService.swift
@@ -0,0 +1,43 @@
+//
+//  UserDataService.swift
+//  Hack&Change23Track1
+//
+//  Created by Bogdan Zykov on 24.11.2023.
+//
+
+import Foundation
+
+class UserDataService {
+    
+    private let api = Network.shared.client
+    
+    func singIn(login: String, password: String) async throws -> String {
+        let mutation = SignInMutation(login: login, password: password)
+        let data = try await api.mutation(mutation: mutation, publishResultToStore: false)
+        guard let token = data.data?.signIn?.token else {
+            throw URLError(.badServerResponse)
+        }
+        print("singIn with", login)
+        return token
+    }
+    
+    func singUp(login: String, password: String) async throws -> String {
+        let mutation = SignUpMutation(login: login, password: password)
+        let data = try await api.mutation(mutation: mutation, publishResultToStore: false)
+        guard let token = data.data?.signUp?.token else {
+            throw URLError(.badServerResponse)
+        }
+        print("singUp with", login)
+        return token
+    }
+    
+    func getCurrentUser() async throws -> UserAttrs {
+        let query = CurrentUserQuery()
+        let data = try await api.fetch(query: query)
+        
+        guard let user = data?.data?.currentUser?.fragments.userAttrs else {
+            throw URLError(.badServerResponse)
+        }
+        return user
+    }
+}
