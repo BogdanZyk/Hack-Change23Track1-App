616d7ea354bf2fe59889fcf0a54373720861a9bb
BogdnaZyk <zykovbogdan07@gmail.com>
2023-11-24 11:09:17 +0300

    Initial Commit

---
+++ Hack&Change23Track1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hack&Change23Track1
+//
+//  Created by Bogdan Zykov on 24.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hack&Change23Track1/Hack_Change23Track1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hack_Change23Track1App.swift
+//  Hack&Change23Track1
+//
+//  Created by Bogdan Zykov on 24.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Hack_Change23Track1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
